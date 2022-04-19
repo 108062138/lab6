edf853545b24274e6878254e8184217fa82d80b0
108062138 <poyuwu17@gmail.com>
2022-04-19 03:58:51 +0100

    initail

---
+++ blink_led.py
@@ -0,0 +1,9 @@
+import RPi.GPIO as GPIO
+from time import sleep
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(2,GPIO.OUT)
+while 1:
+    GPIO.output(2,GPIO.HIGH)
+    sleep(1)
+    GPIO.output(2,GPIO.LOW)
+    sleep(1)
